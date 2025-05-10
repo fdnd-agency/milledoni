974d0798678fb98370300c75afee186c6e469d6b
smolgeorgie <ilona.van.oosbree@hva.nl>
2025-05-10 19:03:57 +0200

    #25 page works, data not loading in

---
+++ src/routes/products/[slug]/+page.server.js
@@ -0,0 +1,9 @@
+import fetchJson from "$lib/fetch-json.js";
+
+export async function load({ params }) {
+    const url = `https://fdnd-agency.directus.app/items/milledoni_products/${params.slug}`
+    const product = await fetchJson(url)
+    return {
+        product: product.data
+    }
+}
---
+++ src/routes/products/[slug]/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let data;
+</script>
+
+<main>
+    <h1>Product Details</h1>
+    {#if data.product}
+        <h2>{data.product.name}</h2>
+        <p>{data.product.description}</p>
+        <p>Price: ${data.product.price}</p>
+        <img src={data.product.image} alt={data.product.name} />
+    {:else}
+        <p>Product not found.</p>
+    {/if}
+</main>
