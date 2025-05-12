f5e5e6aa7bc7caa0494de1b73248f176cb787a34
smolgeorgie <ilona.van.oosbree@hva.nl>
2025-05-12 11:42:12 +0200

    #25 HTML loaded in- Works
    The data is shown on the detail page correctly.
    Close #27

--- src/routes/products/[slug]/+page.server.js
+++ src/routes/products/[slug]/+page.server.js
@@ -1,7 +1,7 @@
 import fetchJson from "$lib/fetch-json.js";
 
 export async function load({ params }) {
-    const url = `https://fdnd-agency.directus.app/items/milledoni_products/${params.slug}`;
+    const url = `https://fdnd-agency.directus.app/items/milledoni_products?filter[slug][_eq]=${params.slug}`;
     
     const product = await fetchJson(url);
 
--- src/routes/products/[slug]/+page.svelte
+++ src/routes/products/[slug]/+page.svelte
@@ -4,11 +4,11 @@
 
 <main>
     <h1>Product Details for {data.slug}</h1>
-    {#if data.product}
-        <h2>{data.product.name}</h2>
-        <p>{data.product.description}</p>
-        <p>Price: ${data.product.price}</p>
-        <img src={data.product.image} alt={data.product.name} />
+    {#if data.product && data.product.length > 0}
+        <h2>{data.product[0].name}</h2>
+        <p>{data.product[0].description}</p>
+        <p>Price: ${data.product[0].price}</p>
+        <img src={data.product[0].image} alt={data.product[0].name} />
     {:else}
         <p>Product not found.</p>
     {/if}
