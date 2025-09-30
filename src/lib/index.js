b5441f62210e1279ecff27af45607956e8ebd057
Colin de Groot <colindegroot2004@gmail.com>
2025-09-30 15:28:16 +0200

    captial error fix

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,4 @@
 // place files you want to import through the `$lib` alias in this folder.
 
 
-export {  default as Product } from './component/Product.svelte'
+export {  default as Product } from './component/product.svelte'
