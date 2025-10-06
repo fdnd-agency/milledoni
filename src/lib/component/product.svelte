982e45325e21b0d427a37c5b1d67b0bff0502a57
Colin de Groot <colindegroot2004@gmail.com>
2025-10-06 19:34:39 +0200

    [FEATURE] begin styling product

--- src/lib/component/product.svelte
+++ src/lib/component/product.svelte
@@ -7,3 +7,23 @@
     <h3>{product.name}</h3>
     <p>{product.description}</p>
 </li>
+
+<style>
+
+			li{
+				border-radius: 5px;
+				width: 22%;
+                background-color: #f6f5f5;
+
+				h3{
+					
+					
+				}
+				
+				p{
+					
+				}
+		}
+		
+		
+</style>
