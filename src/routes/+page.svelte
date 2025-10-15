94fbb7ec7572218b4d41ee1f52a90aeaf4f5f8ea
Colin de Groot <colindegroot2004@gmail.com>
2025-10-15 12:09:00 +0200

    [FEATURE] filters on homepage #95

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,37 @@
 <script>
-    let { data } = $props()
+  let { data } = $props();
 
-    //import products component
-    import { Product } from '$lib'
+  //import products component
+  import { Product } from "$lib";
 
-    const products = data.product
-    
-    
+  const products = data.product;
+  const tags = data.tags;
+
 </script>
 
 <h1>dit is een test</h1>
 
-<ul>
-    {#each products as product}
-        <Product {product}/>
+<form>
+  <label for="tag-select">Kies een tag:</label>
+  <select id="tag-select">
+    <option value="">Alle tags</option>
+    {#each tags as tag}
+      <option value={tag}>{tag}</option>
     {/each}
+  </select>
+</form>
+
+<ul>
+  {#each products as product}
+    <Product {product} />
+  {/each}
 </ul>
 
 <style>
-    ul{
-        list-style: none;
-        display: flex;
-        flex-flow: row wrap;
-        gap: 2em;
-    }
+  ul {
+    list-style: none;
+    display: flex;
+    flex-flow: row wrap;
+    gap: 2em;
+  }
 </style>
