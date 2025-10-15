19c243d59b71f8b255e8b91fac90b2929df30024
Clarice-COD <clariceboerlijst@gmail.com>
2025-10-15 13:28:33 +0200

    inner card aligned and designed

--- src/lib/component/product.svelte
+++ src/lib/component/product.svelte
@@ -31,26 +31,65 @@
 		justify-items: center;
 		place-items: center;
 		text-align: center;
-		padding: 1.50rem;
-		box-sizing: border-box;
 		border: 2px solid hsl(47, 100%, 44%);
 		border-radius: 1rem;
 
 			a {
 				text-decoration: none;
 				color: var(--green-main);
-				color: hsl(167, 100, 26);
+				color: hsl(167, 100%, 26%);
+				color: hsl(167, 100%, 26%);
+				width: 100%;
+				height: 100%;
+				padding: 1.50rem;
+				box-sizing: border-box;
+
+				img {
+					border-radius: 1rem;
+				}
 
 				h3 {
 					color: var(--main-text-color);
-					color: rgb(130, 130, 130);
+					color: #212121;
 					font-size: var(--h3-font-size);
 					max-width: 13rem;
 				}
 			}
 		}
 
+		a {
+			font-family: "Parkisans";
+		}
+
+		form, input, label {
+			cursor: pointer;
+		}
+
 		form {
             display: flex;
+			width: 100%;
+			padding: 1.50rem;
+			padding-top: 0;
+			box-sizing: border-box;
+			gap: 1rem;
         }
+
+		.like-form, .safe-form {
+			width: 45%;
+			height: 2rem;
+			border: 2px solid hsl(167, 100%, 26%);
+			color: hsl(167, 100%, 26%);
+			justify-items: center;
+			align-items: center;
+			display: flex;
+			gap: 10px;
+			padding: 1rem;
+			box-sizing: border-box;
+			border-radius: 0.5rem;
+		}
+
+		input {
+			background-color: hsl(167, 100%, 26%);
+			border: none;
+		}
 </style>
