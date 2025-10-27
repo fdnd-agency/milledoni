af30dc458a7c3e688e75570e1d679416df5149d4
Clarice-COD <clariceboerlijst@gmail.com>
2025-10-27 16:05:11 +0100

    Code structure

--- src/lib/component/product.svelte
+++ src/lib/component/product.svelte
@@ -6,13 +6,13 @@
 	<div>
 		<a href="/"><img loading="lazy" src="{product.image}" alt="test" width="200" height="200"></a>
 		<h3>{product.name}</h3>
-		<a href="/">Bekijk products 
-			<img src="/images/right-arrow.svg" alt="" width="14" height="14" />
+		<a class="bekijk-product-btn" href="/">Bekijk product
+			<img loading="lazy" src="/images/right-arrow.svg" alt="" width="14" height="14" />
 		</a>
 	</div>
-	<form action="post" method="get" class="like-and-safe-form">
-            <button type="submit"><img src="/images/heart-icon.svg" alt="" width="30" height="30" /></button>
-            <label for="like">like</label>
+	<form action="post" method="get">
+		<button type="submit"><img src="/images/heart-icon.svg" alt="" width="30" height="30" /></button>
+		<label for="like">like</label>
     </form>
 </li>
 
@@ -40,58 +40,62 @@
 				color: hsl(167, 100%, 26%);
 				width: 100%;
 				height: 100%;
-
-				img {
-					border-radius: 1rem;
-				}
-
-				h3 {
-					color: var(--main-text-color);
-					color: #212121;
-					font-size: var(--h3-font-size);
-					max-width: 13rem;
-					height: 2.5rem;
-				}
 			}
-		}
 
-		img:hover {
-			scale: 1.02;
-		}
+			a {
+				font-family: "Parkisans";
+				text-decoration: none;
+				color: hsl(167, 100%, 26%);
+				display: flex;
+				align-items: center;
+				gap: 2px;
+			}
 
-		a:hover {
-			font-size: 1.02rem;
-		}
+			a:hover {
+				font-size: 1.1rem;
+			}
 
-		button:hover {
-			scale: 1.1;
-		}
+			.bekijk-product-btn {
+				height: 1.5rem;
+			}
 
-		a {
-			font-family: "Parkisans";
-			text-decoration: none;
-			color: hsl(167, 100%, 26%);
-			display: flex;
-			align-items: center;
-			gap: 2px;
-		}
+			h3 {
+				color: var(--main-text-color);
+				color: #212121;
+				font-size: var(--h3-font-size);
+				max-width: 13rem;
+				height: 2.5rem;
+			}
 
-		form, button, label {
-			cursor: pointer;
-		}
+			img {
+				border-radius: 1rem;
+			}
 
-		form {
-    		justify-items: start;
-    		align-items: center;
-    		display: flex;
-			gap: 5px;
-			width: 100%;
-		}
+			img:hover {
+				scale: 1.02;
+			}
 
-		button {
-			background-color: #00000000;
-			border: none;
-			padding: 0;
-			display: grid;
+			form {
+				justify-items: start;
+				align-items: center;
+				display: flex;
+				gap: 5px;
+				width: 100%;
+			}
+
+			form, button {
+				cursor: pointer;
+			}
+
+			button {
+				background-color: #00000000;
+				border: none;
+				padding: 0;
+				display: grid;
+			}
+
+			button:hover {
+				scale: 1.1;
+			}
 		}
 </style>
