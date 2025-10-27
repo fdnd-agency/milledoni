00cd8c515f83a979e4936b3c1a5650caa67b9912
Clarice-COD <clariceboerlijst@gmail.com>
2025-10-27 21:27:18 +0100

    img like icon changed to svg

--- src/lib/component/product.svelte
+++ src/lib/component/product.svelte
@@ -11,10 +11,15 @@
 			<img loading="lazy" src="/images/right-arrow.svg" alt="" width="14" height="14" />
 		</a>
 	</div>
-	<form action="post" method="get">
-        <button id="likeBtn" type="submit"><img src="/images/heart-icon.svg" alt="" width="30" height="30" /></button>
-        <label for="likeBtn">like</label>
- 	</form>
+	<form action="/" method="post">
+		<button id="likeBtn" type="submit" aria-label="Like">
+		  <svg width="30" height="30" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" fill="#212121">
+			<path d="M10.5,8v2C9.122,10,8,11.121,8,12.5H6C6,10.019,8.019,8,10.5,8z" />
+			<path d="M21.5,5c-2.116,0-4.093,0.881-5.5,2.406C14.593,5.881,12.616,5,10.5,5C6.364,5,3,8.333,3,12.5C3,21.542,16,27,16,27s13-5.458,13-14.5C29,8.333,25.636,5,21.5,5z M16,24.797C13.378,23.521,5,18.938,5,12.5C5,9.467,7.467,7,10.5,7c1.55,0,2.982,0.626,4.03,1.762l0.735,0.797h1.47l0.735-0.797C18.518,7.626,19.95,7,21.5,7c3.033,0,5.5,2.467,5.5,5.5C27,18.938,18.622,23.521,16,24.797z" />
+		  </svg>
+		</button>
+		<label for="likeBtn">like</label>
+	</form>
 </li>
 
 <style>
