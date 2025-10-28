8373d28bc2b1b141930d327a41113e3f8caf9f7b
Colin de Groot <colindegroot2004@gmail.com>
2025-10-28 14:53:20 +0100

    simple format and comments added

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,25 +1,28 @@
 export async function load() {
-   //get products
-    const productData = await fetch('https://fdnd-agency.directus.app/items/milledoni_products');
+  //get products
+  const productData = await fetch(
+    "https://fdnd-agency.directus.app/items/milledoni_products"
+  );
 
-    const productResponse = await productData.json();
-     const products = productResponse.data;
+  const productResponse = await productData.json();
+  const products = productResponse.data;
 
+  const allTags = products.map( //maybe needs flatmap to prevent double arrays?
+    (product) =>
+      product.tags
+        ? product.tags
+            .split(",")
+            .map((tag) => tag.replace(/['\s]/g, " ").trim())
+        : [] // removal of qoutes. That will look bad in the html
+  );
 
-    const allTags = products
-        .flatMap(product => 
-            product.tags ? product.tags.split(',').map(tag => tag.replace(/['\s]/g, '').trim()) : [] // removal of spacebars and qoutes. That will look bad in the html
-        );
+  const uniqueTags = [...new Set(allTags)]; //no duplicate  filters with set()
 
-   
-    const uniqueTags = [...new Set(allTags)]; //no duplicate  filters with set()
+  // console.log(uniqueTags); 
+  //log the tags for debug purpose
 
-
-    console.log(uniqueTags); //log the tags for debug purpose
-
-   
-    return { 
-       product: products,
-        tags: uniqueTags
-    };
+  return {
+    product: products,
+    tags: uniqueTags,
+  };
 }
