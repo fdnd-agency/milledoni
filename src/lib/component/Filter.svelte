a03e8c2812516740d017a97e66772369ae32987f
Colin de Groot <colindegroot2004@gmail.com>
2025-10-29 21:21:56 +0100

    [BUG] filter added in component for better readabillity and data fix
    this fixxes the merge confilct

---
+++ src/lib/component/Filter.svelte
@@ -0,0 +1,148 @@
+<script>
+  let { tags } = $props();
+  
+  import image from "$lib/assets/filter-bg.svg";
+
+</script>
+
+<div class="form-wrapper">
+
+  <form>
+
+    <fieldset>
+      <label for="tag-select">De persoon</label>
+      <select id="tag-select">
+        <option value="">Alle tags</option>
+        {#each tags as tag}
+          <option value={tag}>{tag}</option>
+        {/each}
+      </select>
+    </fieldset>
+
+    <fieldset>
+      <label for="tag-select">Deze persoon is</label>
+      <select id="tag-select">
+        <option value="">Alle tags</option>
+        {#each tags as tag}
+          <option value={tag}>{tag}</option>
+        {/each}
+      </select>
+    </fieldset>
+
+    <fieldset>
+      <label for="tag-select">Stijl & voorkeuren</label>
+      <select id="tag-select">
+        <option value="">Alle tags</option>
+        {#each tags as tag}
+          <option value={tag}>{tag}</option>
+        {/each}
+      </select>
+    </fieldset>
+
+  </form>
+
+  <img class="svg" src={image} alt="" />
+</div>
+
+
+<style>
+  .form-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    margin: auto;
+    width: 90%;
+    position: relative;
+
+    background-color: #20a687;
+    border-radius: 15px;
+
+    z-index: 2;
+    padding: 1em;
+
+    @media screen and (min-width: 1030px){
+        width: 60%;
+        padding: 3em;
+      }
+    
+
+    form {
+      display: flex;
+      flex-flow: column wrap;
+      justify-content: space-evenly;
+      
+      gap: 1.5em;
+
+      width: 100%;
+      height: 80%;
+      margin: auto;
+
+      /* relative for z-index */
+      position: relative;
+      z-index: 2;
+
+      padding: 1em;
+      
+      border: 5px solid #20a687;
+      border-radius: 15px;
+
+      background-color: #fff;
+      height: auto;
+
+      margin: auto;
+
+      @media screen and (min-width: 1075px){
+        flex-direction: row;
+      }
+
+      fieldset {
+        display: flex;
+        flex-direction: column;
+
+        gap: 0.5em;
+
+        border: none;
+        padding: 0;
+        margin: 0;
+      }
+
+      label {
+        color: #20a687;
+        font-weight: bold;
+      }
+
+      select {
+        font-family: inherit;
+        width: 100%;
+
+        border-radius: 5px;
+        border: 2px solid lightgray;
+        color: #1d1d1b;
+        background-color: #fff;
+        padding: 5px;
+
+        /* option {
+          &::checked {
+          }
+        } */
+      }
+    }
+  }
+
+  .svg {
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+
+    border-radius: 15px;
+
+    z-index: 1;
+  }
+
+  
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,52 +4,13 @@
 
   //import products component
   import { Product } from "$lib";
+  import { Filter } from "$lib";
 
   const products = data.product;
   const tags = data.tags;
 </script>
 
-<h1>Filter test</h1>
-
-<div class="form-wrapper">
-  `
-
-  <form>
-
-    <fieldset>
-      <label for="tag-select">De persoon</label>
-      <select id="tag-select">
-        <option value="">Alle tags</option>
-        {#each tags as tag}
-          <option value={tag}>{tag}</option>
-        {/each}
-      </select>
-    </fieldset>
-
-    <fieldset>
-      <label for="tag-select">Deze persoon is</label>
-      <select id="tag-select">
-        <option value="">Alle tags</option>
-        {#each tags as tag}
-          <option value={tag}>{tag}</option>
-        {/each}
-      </select>
-    </fieldset>
-
-    <fieldset>
-      <label for="tag-select">Stijl & voorkeuren</label>
-      <select id="tag-select">
-        <option value="">Alle tags</option>
-        {#each tags as tag}
-          <option value={tag}>{tag}</option>
-        {/each}
-      </select>
-    </fieldset>
-
-  </form>
-
-  <img class="svg" src={image} alt="" />
-</div>
+<Filter {tags} />
 
 <ul>
   {#each products as product}
@@ -58,108 +19,11 @@
 </ul>
 
 <style>
-  .form-wrapper {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    margin: auto;
-    width: 90%;
-    position: relative;
-
-    background-color: #20a687;
-    border-radius: 15px;
-
-    z-index: 2;
-    padding: 1em;
-
-    @media screen and (min-width: 1030px){
-        width: 60%;
-        padding: 3em;
-      }
-    
-
-    form {
-      display: flex;
-      flex-flow: column wrap;
-      justify-content: space-evenly;
-      
-      gap: 1.5em;
-
-      width: 100%;
-      height: 80%;
-      margin: auto;
-
-      /* relative for z-index */
-      position: relative;
-      z-index: 2;
-
-      padding: 1em;
-      
-      border: 5px solid #20a687;
-      border-radius: 15px;
-
-      background-color: #fff;
-      height: auto;
-
-      margin: auto;
-
-      @media screen and (min-width: 1075px){
-        flex-direction: row;
-      }
-
-      fieldset {
-        display: flex;
-        flex-direction: column;
-
-        gap: 0.5em;
-
-        border: none;
-        padding: 0;
-        margin: 0;
-      }
-
-      label {
-        color: #20a687;
-        font-weight: bold;
-      }
-
-      select {
-        font-family: inherit;
-        width: 100%;
-
-        border-radius: 5px;
-        border: 2px solid lightgray;
-        color: #1d1d1b;
-        background-color: #fff;
-        padding: 5px;
-
-        /* option {
-          &::checked {
-          }
-        } */
-      }
-    }
-  }
-
-  .svg {
-    position: absolute;
-    top: 0;
-    left: 0;
-
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-
-    border-radius: 15px;
-
-    z-index: 1;
-  }
-
   ul {
     list-style: none;
     display: flex;
     flex-flow: row wrap;
+    justify-content: center;
     gap: 2em;
   }
 </style>
