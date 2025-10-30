14f2b518a00409f9152f8940f88e1ee2d44522b6
Anouarab2 <anouar.ait.brahim@hva.nl>
2025-10-30 12:14:07 +0100

    feature HTML detailpage #128

--- src/routes/[name]/+page.svelte
+++ src/routes/[name]/+page.svelte
@@ -1,1 +1,43 @@
-<h1>detailpage</h1>
+<main>
+    <a href="/">Go back</a>
+    <img src="" alt="">
+    <h1>Una bicicletta usata da restaurare insieme</h1>
+    <p>La Gilberti X-Trail 500 è una mountain bike progettata per avventure estreme. Telaio in alluminio leggero, sospensioni anteriori da una mountain bike progettata per avventure estreme.
+        Bike progettata per avventure estreme. Telaio in alluminio leggero, sospensioni anteriori da una mountain bike progettata per avventure estreme.</p>
+
+    <section>
+        <div>
+            <!-- <svg></svg> -->
+            <h2>Shop online</h2>
+            <p>A few clicks away</p>
+        </div>
+        <div>
+            <p>Decathlon - 350,99 €</p>
+        </div>
+        <div>
+            <p>Sportmax - 367,89 €</p>
+        </div>
+    </section>    
+
+    <section>
+        <h2>find it near you</h2>
+        <img src="" alt="">
+        <!-- <svg></svg> -->
+    </section>
+
+    <section>
+        <h2>you may also like</h2>
+
+        <article>
+            product
+        </article>
+        <article>
+            product
+        </article>
+        <article>
+            product
+        </article>
+
+        <a href="/">see more</a>
+    </section>
+</main>
