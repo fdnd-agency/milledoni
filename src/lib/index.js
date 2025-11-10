2ecffca166b0655f1c769167d84484e238c31bb4
Colin de Groot <colindegroot2004@gmail.com>
2025-11-10 15:04:18 +0100

    [FIX] Rewrite header route in lib for easier import

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
 
-
-export {  default as Product } from './component/product.svelte'
+export {  default as Header } from './component/Header.svelte'
+export {  default as Product } from './component/Product.svelte'
 export {  default as Filter } from './component/Filter.svelte'
