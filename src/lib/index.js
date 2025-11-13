8f3e88449da2de31facb0c686c1b908f471f86c4
Colin de Groot <colindegroot2004@gmail.com>
2025-11-13 13:11:13 +0100

    Update index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
+// make sure components start with a capital letter in both the route and naming during export
 
 export {  default as Header } from './component/Header.svelte'
 export {  default as Product } from './component/Product.svelte'
