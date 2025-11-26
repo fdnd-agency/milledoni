a40ed6de9631730621a399c1dec597bdc88056ce
Clarice-COD <clariceboerlijst@gmail.com>
2025-11-26 14:48:14 +0100

    add: uppercase
    #162

--- src/lib/component/AccountLink.svelte
+++ src/lib/component/AccountLink.svelte
@@ -1,6 +1,7 @@
 <script>
     import icon from "$lib/assets/account-icon.svg";
 </script>
+
 <div class="account-container">
     <a href="/" aria-label="jouw profiel">
         <div class="symbol-account">
@@ -16,28 +17,30 @@
     }
 
     a {
-
         display: inline-flex;
+        text-decoration: none;
     }
 
     .symbol-account {
         display: flex;
         align-items: center;
-        gap: 0.5rem;
+        gap: 0.8rem;
         border-radius: var(--rounding-circle);
         color: white;
         background-color: var(--accent-color);
-        padding: var(--spacing-s) var(--spacing-m) var(--spacing-s) var(--spacing-m);
+        padding: var(--spacing-s);
     }
 
     @container (width > 350px) {
 
         .symbol-account::after {
             content: "account";
+            text-transform: uppercase;
         }
 
         .symbol-account {
             border-radius: var(--rounding-l);
+            padding: var(--spacing-s) var(--spacing-m) var(--spacing-s) var(--spacing-m);
         }
     }
 </style>
