7dd6973fce86acfca2fc9106f69331a276eb8f81
Clarice-COD <clariceboerlijst@gmail.com>
2025-12-04 17:29:47 +0100

    add: clock placement
    #171

--- src/routes/scroll-driven-animation/+page.svelte
+++ src/routes/scroll-driven-animation/+page.svelte
@@ -8,4 +8,23 @@
     import background from '$lib/assets/gear-background.jpg';
 </script>
 
-<h1>test</h1>
+<main>
+    <img alt="pendulum clock" class="clock" width="300px" height="300px" src={clock} />
+</main>
+
+<style>
+    main {
+        margin:0;
+        height: 100vh;
+        width: 100vw;
+        background-color: rgb(190, 190, 190);
+        z-index: auto;
+    }
+
+    .clock {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+</style>
