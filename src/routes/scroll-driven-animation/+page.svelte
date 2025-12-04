5a97b254129ce05c3a04082ab6363241eab90e9f
Clarice-COD <clariceboerlijst@gmail.com>
2025-12-04 16:47:50 +0100

    add: imported images
    #171

--- src/routes/scroll-driven-animation/+page.svelte
+++ src/routes/scroll-driven-animation/+page.svelte
@@ -1,1 +1,11 @@
+<script>
+	import clock from '$lib/assets/clock.png';
+    import gear1 from '$lib/assets/gear1.png';
+    import gear2 from '$lib/assets/gear2.png';
+    import gear3 from '$lib/assets/gear3.png';
+    import gear4 from '$lib/assets/gear4.png';
+    import gear5 from '$lib/assets/gear5.png';
+    import background from '$lib/assets/gear-background.jpg';
+</script>
+
 <h1>test</h1>
