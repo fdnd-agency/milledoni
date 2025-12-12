bd3bdbb06958c8bc11dea08a4aa07b4d9b0f43ae
Colin de Groot <colindegroot2004@gmail.com>
2025-12-12 11:51:16 +0100

    [feature]sibling count, scroll buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,49 +6,50 @@
 </script>
 
 <main id="content">
-  <h1>hoi</h1>
+  <h1>Holographic carousel</h1>
+  <p>Met gebruik van css scrollbuttons, acnhor positioning en sibling count</p>
 
   <div class="slider">
     <article style="background-image: url({holo_bg})">
       <div class="glass">
-        <img src="" alt="nog niks" />
+        <img src={holo_star} alt="nog niks" />
 
         <div class="description">
-          <h3>Product</h3>
-          <p>small description</p>
+          <h3>Holo ster</h3>
+          <p>een mooie holograpische ster</p>
         </div>
       </div>
     </article>
 
     <article style="background-image: url({holo_bg})">
       <div class="glass">
-        <img src="" alt="nog niks" />
+        <img src={holo_star} alt="nog niks" />
 
         <div class="description">
-          <h3>Product</h3>
-          <p>small description</p>
+          <h3>Holo ster</h3>
+          <p>een mooie holograpische ster</p>
         </div>
       </div>
     </article>
 
     <article style="background-image: url({holo_bg})">
       <div class="glass">
-        <img src="" alt="nog niks" />
+        <img src={holo_star} alt="nog niks" />
 
         <div class="description">
-          <h3>Product</h3>
-          <p>small description</p>
+          <h3>Holo ster</h3>
+          <p>een mooie holograpische ster</p>
         </div>
       </div>
     </article>
 
     <article style="background-image: url({holo_bg})">
       <div class="glass">
-        <img src="" alt="nog niks" />
+        <img src={holo_star} alt="nog niks" />
 
         <div class="description">
-          <h3>Product</h3>
-          <p>small description</p>
+          <h3>Holo ster</h3>
+          <p>een mooie holograpische ster</p>
         </div>
       </div>
     </article>
@@ -71,23 +72,37 @@
     box-sizing: border-box !important;
   }
 
+  main {
+    width: 80vw;
+    margin: 10vh auto;
+  }
+
   .slider {
     display: flex;
-    overflow-x: scroll;
+    overflow-x: auto;
+    margin: 10vh 0;
     gap: 3vw;
     scroll-snap-type: x mandatory;
+    scroll-behavior: smooth;
+    anchor-name: --holocarousel;
+    padding-right: 5vw;
+    position: relative;
   }
 
   article {
-    height: 50vh;
-    flex: 0 0 20em;
+    height: 60vh;
+    flex: 0 0 25em;
+    margin: 0 auto;
     border-radius: 0.5em;
     padding: 1em;
 
-    background-size: 250% 250%;
-    animation: moveBg 10s linear infinite alternate;
+    background-size: calc(250% + sibling-index() * 20%)
+      calc(250% + sibling-index() * 20%);
+    animation: moveBg linear infinite alternate;
+    animation-duration: calc(15s + sibling-index() * 1.5s);
 
-
+    scroll-snap-align: start;
+    scroll-snap-stop: always;
 
     .glass {
       display: flex;
@@ -100,7 +115,6 @@
       border-radius: 0.5em;
       box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
       backdrop-filter: blur(4.6px);
-      -webkit-backdrop-filter: blur(4.6px);
     }
 
     img {
@@ -125,6 +139,57 @@
       }
     }
   }
+
+  .slider::-webkit-scrollbar {
+    opacity: 0.1;
+  }
+
+
+  /* scroll buttons */
+  /* tijdelijk onderaan voor beter overzicht */
+  .slider::scroll-button(*) {
+    position: absolute;
+    content: "<";
+    border: none;
+    background-color: #111112;
+    color: #fff;
+    font-size: 2rem;
+    height: 30px;
+    width: 30px;
+    border-radius: 50%;
+    cursor: pointer;
+    z-index: 999;
+    position-area: left center;
+    position-anchor: --holocarousel;
+
+    @media (min-width: 1100px) {
+      font-size: 3rem;
+      width: 50px;
+      height: 50px;
+    }
+  }
+
+  .slider::scroll-button(right) {
+    content: ">";
+    position-area: right center;
+    right: 0;
+  }
+
+  .slider::scroll-button(*):disabled {
+    opacity: 0.2;
+    cursor: auto;
+  }
+
+  /* deze twee verpesten anders de slider om een of andere reden */
+  .slider::scroll-button(block-start) {
+    display: none;
+  }
+
+  .slider::scroll-button(block-end) {
+    display: none;
+  }
+
+  
 
   @keyframes moveBg {
     0% {
