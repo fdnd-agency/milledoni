b5b01b6e7640d7378f2d15141aae6a57ad5757dc
Clarice-COD <clariceboerlijst@gmail.com>
2025-12-17 17:07:35 +0100

    add: smaller menu on desktop
    #176

--- src/lib/component/MenuButton.svelte
+++ src/lib/component/MenuButton.svelte
@@ -1,15 +1,17 @@
 <script>
     import icon from "$lib/assets/menu-icon.svg";
+    let open = false;
 </script>
+
 
 <details>
     <summary>
-        <img class="svg" src={icon} alt="account" />
+        <img src={icon} alt="account" />
     </summary>
     <nav>
-        <a href="">home</a>
-        <a href="">about</a>
-        <a href="">spotters</a>
+            <a href="">home</a>
+            <a href="">about</a>
+            <a href="">spotters</a>
     </nav>
 </details>
 
@@ -24,16 +26,8 @@
         z-index: 200;
     }
 
-    nav {
-        z-index: 100;
-    }
-
     img{
         margin: auto;
-    }
-
-    summary::marker {
-        content: url("/src/lib/assets/menu-icon.svg") / "menu";
     }
 
     summary {
@@ -52,11 +46,19 @@
         flex-direction: column;
         position: fixed;
         top: 0;
-        left: 0;
+        right: 0;
         width: 100vw;
         height: 100vh;
         background-color: var(--accent-color);
         justify-content: center;
+        z-index: 100;
+
+        @media (min-width: 970px) {
+            width: fit-content;
+            height: auto;
+            top: 6rem;
+            position: absolute;
+        }
     }
 
     a {
@@ -65,5 +67,31 @@
         text-decoration: none;
         text-align: center;
         font-size: var(--text-font-size-l);
+
+        @media (min-width: 970px) {
+            color: rgb(145, 145, 145);
+            background-color: var(--neutral-color-background-menu);
+            border-bottom: 2px solid rgb(203, 203, 203);
+            font-size: var(--text-font-size-s);
+        }
     }
+
+    @container (width > 350px) {
+
+        summary::marker {
+            content: "Menu";
+        }
+
+        summary {
+            border-radius: var(--rounding-l);
+            width: auto;
+            height: auto;
+            padding: var(--spacing-s) var(--spacing-m) var(--spacing-s) var(--spacing-m);
+        }
+
+        img {
+            width: 1.5rem;
+            height: 1.5rem;
+        }
+}
   </style>
