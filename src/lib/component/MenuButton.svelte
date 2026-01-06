71d1eb18bc34da8f9d403802f2bd46cc6996920e
Clarice-COD <clariceboerlijst@gmail.com>
2026-01-06 14:16:46 +0100

    fix: the base of the hamburger menu
    #182

--- src/lib/component/MenuButton.svelte
+++ src/lib/component/MenuButton.svelte
@@ -1,4 +1,116 @@
 <script>
+	let showMenu = 'translateX(100%)'
+	let crossIcon = 'none'
+	let hamburgerIcon = 'block'
+	let toggleMenu = () => {
+		if ( showMenu == 'translateX(100%)' ) {
+			showMenu = 'translateX(0)'
+			hamburgerIcon = 'none'
+			crossIcon = 'block'
+		} else {
+			showMenu = 'translateX(100%)'
+			hamburgerIcon = 'block'
+			crossIcon = 'none'
+		}
+	}
+</script>
+
+<svelte:head>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</svelte:head>
+
+<ul class="menu" style="--show-menu: {showMenu}">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Spotters</a></li>
+</ul>
+
+<button class="hamburger" on:click={toggleMenu}>
+    <i class="hamburger-icon material-icons" style="--hamburger-icon: {hamburgerIcon}">menu</i>
+    <i class="cross-icon material-icons" style="--cross-icon: {crossIcon}">close</i>
+</button>
+
+<style>
+    .hamburger {
+        position: fixed;
+        z-index: 100;
+        top: 1rem;
+        right: 1rem;
+        padding: 4px;
+        /* border: black solid 1px; */
+        background: white;
+        cursor: pointer;
+    }
+    
+.hamburger-icon {
+  display: var(--hamburger-icon);
+}
+.cross-icon {
+  display: var(--cross-icon);
+}
+.menu {
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+
+    transform: var(--show-menu);
+    transition: transform 0.2s;
+
+    top: 0;
+    right: 0;
+
+    background-color: var(--accent-color);
+    justify-content: center;
+    z-index: 100;
+
+    list-style: none;
+    padding-top: 4rem;
+    width: 50vw;
+    height: 100vh;
+
+    overflow: hidden;
+        padding: 0;
+        margin: 0;
+        -webkit-transition: all 0.3s ease;
+}
+
+ /* ul {
+        display: flex;
+        flex-direction: column;
+        position: fixed;
+        top: 0;
+        right: 0;
+        background-color: var(--accent-color);
+        justify-content: center;
+        z-index: 100;
+        overflow: hidden;
+        max-width: 0;
+        padding: 0;
+        margin: 0;
+        -webkit-transition: all 0.3s ease;
+        width: 100vw;
+        height: 100vh;
+    } */
+
+li {
+        text-align: center;
+        padding: var(--spacing-s);
+    }
+
+a {
+	padding: var(--spacing-m);
+    color: var(--text-color-button);
+    text-decoration: none;
+    text-align: center;
+    font-size: var(--text-font-size-l);
+}
+
+</style>
+
+
+
+
+<!-- <script>
     import icon from "$lib/assets/menu-icon.svg";
     let open = false;
 </script>
@@ -102,4 +214,4 @@
         height: 1.5rem;
     }
     }
-  </style>
+  </style> -->
