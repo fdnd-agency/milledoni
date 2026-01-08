78ac28cbe9963464b6e1b636d71412c2a062c0cd
Colin de Groot <colindegroot2004@gmail.com>
2026-01-08 15:43:01 +0100

    [BUG] fallback for no page url #184

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,11 +1,18 @@
 export async function load({ url }) {
 
 // for now filter will work on /page
-  const page = Number(url.searchParams.get("page"));
+// || 1 so that you will always start at the firstpage. Starting on the homepage with no page url will otherwise result in null
+  const page = Number(url.searchParams.get("page")) || 1;
 
   // number of products shown on the page
   const limit = 15;
+
+  //https://directus.io/docs/guides/connect/query-parameters#offset
+  // -1 so that the offset doesnt skip the products on first page
   const offset = (page - 1) * limit;
+
+  //offset -> items i do not want to shown
+  //limit -> items i do want to show
 
   const productData = await fetch(
     `https://fdnd-agency.directus.app/items/milledoni_products?limit=${limit}&offset=${offset}`
