46e334e77554057d47ca79b242b69e9db6f37545
Colin de Groot <colindegroot2004@gmail.com>
2026-01-08 15:36:59 +0100

    [FEATURE] first pagination test #184
    pagination works on url (/?page=<pagenumber>)

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,26 +1,36 @@
-export async function load() {
-  //get products
+export async function load({ url }) {
+
+// for now filter will work on /page
+  const page = Number(url.searchParams.get("page"));
+
+  // number of products shown on the page
+  const limit = 15;
+  const offset = (page - 1) * limit;
+
   const productData = await fetch(
-    "https://fdnd-agency.directus.app/items/milledoni_products"
-  )
+    `https://fdnd-agency.directus.app/items/milledoni_products?limit=${limit}&offset=${offset}`
+  );
 
   const productResponse = await productData.json();
-  const products = productResponse.data;
+  const products = productResponse.data
 
-  const allTags = products.flatMap(
-    // with map you get verry ongs arrays for some reason. flatmap puts each item seperatly
-    (product) =>
-      product.tags
-        ? product.tags
-            .split(",")
-            .map((tag) => tag.replace(/['\s]/g, " ").trim())
-        : [] // removal of qoutes. That will look bad in the html
-  )
+  // with map you get very longs arrays for some reason. flatmap returns each item seperatly
+  const allTags = products.flatMap((product) =>
+    product.tags
+  //removal of commas and qoutes for clean text in HTML
+      ? product.tags
+          .split(",")
+          .map((tag) => tag.replace(/['\s]/g, " ").trim())
+      : []
+  );
 
-  const uniqueTags = [...new Set(allTags)]; //no duplicate  filters with set()
+  // this removes duplicate tagsfrom being shown in the html
+  // The spread operator is used to grab all excisting tags and remove the duplicate ones
+  const uniqueTags = [...new Set(allTags)];
 
   return {
     product: products,
     tags: uniqueTags,
+    page
   };
 }
