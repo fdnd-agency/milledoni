afd352fa099fc9ed3889ae0b0bbb36f5fe6e7386
Clarice-COD <clariceboerlijst@gmail.com>
2026-01-10 17:16:56 +0100

    add: post request for likes
    #138

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -10,3 +10,22 @@ export async function load({ fetch }) {
 		likes: likesResponse.data
 	};
 }
+
+// Posting likes
+export const actions = {
+	like: async ({ request, fetch }) => {
+		const data = await request.formData();
+		const productId = data.get('productId');
+
+		await fetch('https://fdnd-agency.directus.app/items/milledoni_users', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				name: 'Milledoni',
+	            liked_products: [productId]
+			})
+		});
+
+		return { success: true };
+	}
+};
